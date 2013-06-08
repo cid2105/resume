cdbf76e6c87cf8569104fc19860f7f738c31dec7
cid2105 <cd1am0nd@yahoo.com>
2013-06-07 23:37:33 -0400

    asdf

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
-Resume::Application.config.session_store :cookie_store, key: '_resume_session'
+Resume::Application.config.session_store :cookie_store, key: '_resume_session', :domain => :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 # Resume::Application.config.session_store :active_record_store
+
+
+
+# change top level domain size
+Resume::Application.config.session_store :cookie_store, :key => '_blogs_session', :domain => "colediamond.wordpress.com"
--- config/routes.rb
+++ config/routes.rb
@@ -56,4 +56,7 @@ Resume::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  constraints(Subdomain) do
+    match '/' => 'blogs#show'
+  end
 end
